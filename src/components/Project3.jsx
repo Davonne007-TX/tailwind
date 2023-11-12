69c7ce40018162ac1914acbe12763e6215da3b1c
Vonney <davonnejv@gmail.com>
2023-11-11 21:03:06 -0600

    adding paragraphs with lorem ipsum

--- src/components/Project3.jsx
+++ src/components/Project3.jsx
@@ -17,7 +17,27 @@ export default function Project3() {
 
       <div className="grid grid-cols-2 mt-60 p-10 gap-10 w-3/5 ml-auto mr-auto">
         <img src="/images/lawyers.jpg" />
+
+        <p className="font-serif">
+          Nunc eu tempus nisl. Vivamus mattis quam vitae magna fermentum
+          interdum. In vel tristique est, vitae luctus tellus. Proin ornare enim
+          ut neque lobortis, ac aliquam nisl tincidunt. Mauris elit magna,
+          volutpat ut augue in, lacinia commodo ex. Ut mattis nisl purus, vitae
+          tristique massa tincidunt id. In gravida magna ac sem hendrerit
+          elementum. Etiam vel ligula non elit vulputate accumsan in eu nisi.
+        </p>
         <img src="/images/gavel.jpg" />
+
+        <p className="font-serif">
+          {" "}
+          Integer nec porttitor est. Phasellus facilisis egestas libero sed
+          varius. Sed semper nibh quis metus finibus, in blandit lectus
+          elementum. Nam in felis efficitur, tincidunt orci et, congue neque.
+          Integer non finibus libero. Phasellus placerat lectus nibh, at
+          venenatis risus suscipit sit amet. Mauris sit amet dui blandit,
+          interdum felis eu, mollis diam. Maecenas rhoncus finibus purus ut
+          vulputate.
+        </p>
       </div>
     </>
   );
